b1d9f1f4dabcb5215dbb669f47eba3edb79243f7
Dielkis Gutierrez <dielkiag@gmail.com>
2023-05-01 18:33:03 -0300

    corregida3erPreEntrega

--- codigojava/codigo.js
+++ codigojava/codigo.js
@@ -1,38 +1,12 @@
 let carrito = [];
 
 document.addEventListener("DOMContentLoaded", () => {
-    const productosLocal = JSON.parse(localStorage.getItem("carrito"));
-    if (productosLocal != null) {
-      carrito = productosLocal;
-    }
-    mostrarCarrito();
-  });
-  
-class Producto {
-    id;
-    nombre;
-    precio;
-    cantidad;
-    
-constructor (id,nombre,precio,cantidad){
-    this.id = id
-    this.nombre = nombre
-    this.precio = precio
-    this.cantidad = cantidad
-    
-}
-    subTotal = () => {
-        return this.precio*this.cantidad;
-    }
-}
-
-const productos = [
-    new Producto(1, "Rolineras", 500, 1),
-    new Producto(2, "Valvulas", 25, 1),
-    new Producto(3, "Eje trasero", 196.4, 1),
-];
-
-
+  const productosLocal = JSON.parse(localStorage.getItem("carrito"));
+  if (productosLocal != null) {
+    carrito = productosLocal;
+  }
+  mostrarCarrito();
+});
 
 // capturo el id de los botones
 
@@ -40,59 +14,68 @@ const boton1 = document.getElementById("boton1");
 const boton2 = document.getElementById("boton2");
 const boton3 = document.getElementById("boton3");
 
+//Agrega el producto 1 a localStorage
 boton1.addEventListener("click", () => {
-    const producto = productos.find((item) => {
-        return item.id === +boton1.dataset.id
-    });
-    carrito.push(producto);
-    localStorage.setItem("carrito", JSON.stringify(carrito));
-    mostrarCarrito();
+  const producto = productos.find((item) => {
+    return item.id === +boton1.dataset.id;
+  });
+  carrito.push(producto);
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+  mostrarCarrito();
 });
 
+//Agrega el producto 2 a localStorage
 boton2.addEventListener("click", () => {
-    const producto = productos.find((item) => {
-        return item.id === +boton2.dataset.id
-    });
-    carrito.push(producto);
-    localStorage.setItem("carrito", JSON.stringify(carrito));
-    mostrarCarrito();
+  const producto = productos.find((item) => {
+    return item.id === +boton2.dataset.id;
+  });
+  carrito.push(producto);
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+  mostrarCarrito();
 });
 
+//Agrega el producto 3 a localStorage
 boton3.addEventListener("click", () => {
-    const producto = productos.find((item) => {
-        return item.id === +boton3.dataset.id
-    });
-    carrito.push(producto);
-    localStorage.setItem("carrito", JSON.stringify(carrito));
-    mostrarCarrito();
+  const producto = productos.find((item) => {
+    return item.id === +boton3.dataset.id;
+  });
+  carrito.push(producto);
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+  mostrarCarrito();
 });
 
+//
 function mostrarCarrito() {
-    const tabla = document.getElementById("producto");
-    tabla.innerHTML = ``;
-    let counter = 1;
-  
-    carrito.forEach((producto) => {
-        tabla.innerHTML += `
+  const tabla = document.getElementById("producto");
+  tabla.innerHTML = ``;
+  let counter = 1;
+  let productosEnStorage = JSON.parse(localStorage.getItem("carrito"));
+
+  productosEnStorage.forEach((producto) => {
+    tabla.innerHTML += `
             <tr>
               <th>${counter}</th>
               <td>${producto.id}</td>
               <td>${producto.nombre}</td>
-              <td>${producto.precio}</td>
+              <td>$${producto.precio}</td>
               <td>${producto.cantidad}</td>
+              
             </tr>
           `;
-          counter++;
-    });
-    tr = document.createElement("tr");
-    tr.innerHTML = `<th></th>
+    counter++;
+  });
+  tr = document.createElement("tr");
+  tr.innerHTML = `<th>TOTAL</th>
                       <td></td>
                       <td></td>
+                      <td>$${carrito.reduce(
+                        (total, item) =>
+                          total + (item && item.precio ? item.precio : 0),
+                        0
+                      )}</td>
                       <td></td>
-                      <td></td>
-                      <td>${carrito.reduce((total, item) => total + (item && item.precio ? item.precio : 0),0)}</td>
                     </tr>`;
-    tabla.appendChild(tr);
-  }
-  
-  mostrarCarrito();
+  tabla.appendChild(tr);
+}
+
+mostrarCarrito();
