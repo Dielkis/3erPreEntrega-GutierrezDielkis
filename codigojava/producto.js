e0d0b9ea8a7230441660704e626d9827b8590088
Dielkis Gutierrez <dielkiag@gmail.com>
2023-05-01 18:49:34 -0300

    corregido detalles de entrega

--- codigojava/producto.js
+++ codigojava/producto.js
@@ -1,23 +1,22 @@
 class Producto {
-    id;
-    nombre;
-    precio;
-    cantidad;
-  
-    constructor(id, nombre, precio, cantidad) {
-      this.id = id;
-      this.nombre = nombre;
-      this.precio = precio;
-      this.cantidad = cantidad;
-    }
-    subTotal = () => {
-      return this.precio * this.cantidad;
-    };
+  id;
+  nombre;
+  precio;
+  cantidad;
+
+  constructor(id, nombre, precio, cantidad) {
+    this.id = id;
+    this.nombre = nombre;
+    this.precio = precio;
+    this.cantidad = cantidad;
   }
-  
-  const productos = [
-    new Producto(1, "Rolineras", 500, 1),
-    new Producto(2, "Valvulas", 25, 1),
-    new Producto(3, "Eje trasero", 196.4, 1),
-  ];
-  
+  subTotal = () => {
+    return this.precio * this.cantidad;
+  };
+}
+
+const productos = [
+  new Producto(1, "Rolineras", 500, 1),
+  new Producto(2, "Valvulas", 25, 1),
+  new Producto(3, "Eje trasero", 196.4, 1),
+];
