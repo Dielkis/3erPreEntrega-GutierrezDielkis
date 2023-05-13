786980bf6ff75b37e0ed19b9ca21acf857fe7fc8
Dielkis Gutierrez <dielkiag@gmail.com>
2023-05-13 00:32:59 -0300

    modificandoestilos

--- codigojava/codigo.js
+++ codigojava/codigo.js
@@ -13,27 +13,63 @@ btnsAgregar.forEach((btn) => {
 
 function mostrarCarrito() {
   const carritoContainer = document.getElementById("carrito");
-  carritoContainer.innerHTML = "";
+  const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
-  // Obtener productos del carrito desde el localStorage
-  const productosEnStorage = JSON.parse(localStorage.getItem("carrito")) || [];
+  if (carrito.length === 0) {
+    carritoContainer.innerHTML = `<p>El carrito está vacío</p>`;
+    return;
+  }
 
-  // Mostrar productos del carrito
-  productosEnStorage.forEach((producto) => {
-    const productoHTML = `
-      <div>
-        <h3>${producto.nombre}</h3>
-        <p>Precio: ${producto.precio}</p>
-      </div>
-    `;
-    carritoContainer.innerHTML += productoHTML;
-  });
-
-  // Mostrar total de la compra
-  const totalHTML = `<p>Total: ${calcularTotal(productosEnStorage)}</p>`;
-  carritoContainer.innerHTML += totalHTML;
+  carritoContainer.innerHTML = `
+    <table>
+      <thead>
+        <tr>
+          <th>Producto</th>
+          <th>Precio</th>
+          <th>Cantidad</th>
+          <th>Total</th>
+        </tr>
+      </thead>
+      <tbody>
+        ${carrito
+          .map(
+            (item) => `
+            <tr>
+              <td>${item.nombre}</td>
+              <td>${item.precio}</td>
+              <td>${item.cantidad}</td>
+              <td>${item.precio * item.cantidad}</td>
+            </tr>
+          `
+          )
+          .join("")}
+      </tbody>
+      <tfoot>
+        <tr>
+          <td colspan="3">Total</td>
+          <td>${carrito.reduce(
+            (total, item) => total + item.precio * item.cantidad,
+            0
+          )}</td>
+        </tr>
+      </tfoot>
+    </table>
+  `;
 }
 
 function calcularTotal(productos) {
   return productos.reduce((total, producto) => total + producto.precio, 0);
+}
+function agregarAlCarrito(id, nombre, precio) {
+  let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+  let producto = carrito.find((item) => item.id === id);
+
+  if (producto) {
+    producto.cantidad++;
+  } else {
+    carrito.push({ id, nombre, precio, cantidad: 1 });
+  }
+
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+  mostrarCarrito();
 }
