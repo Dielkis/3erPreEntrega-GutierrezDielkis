2b8f9677fffe32d660b624e607cddb3e64147374
Dielkis Gutierrez <dielkiag@gmail.com>
2023-05-13 00:26:05 -0300

    otrodiseñodecarrito

--- codigojava/codigo.js
+++ codigojava/codigo.js
@@ -1,96 +1,39 @@
-let carrito = [];
+const btnsAgregar = document.querySelectorAll(".btn-agregar");
+const carrito = [];
 
-document.addEventListener("DOMContentLoaded", () => {
-  const productosLocal = JSON.parse(localStorage.getItem("carrito"));
-  if (productosLocal != null) {
-    carrito = productosLocal;
-  }
-  mostrarCarrito();
-});
-
-
-// // capturo el id de los botones
-
-// const boton1 = document.getElementById("boton1");
-// const boton2 = document.getElementById("boton2");
-// const boton3 = document.getElementById("boton3");
-
-// //Agrega el producto 1 a localStorage
-// boton1.addEventListener("click", () => {
-//   const producto = productos.find((item) => {
-//     return item.id === +boton1.dataset.id;
-//   });
-//   carrito.push(producto);
-//   localStorage.setItem("carrito", JSON.stringify(carrito));
-//   mostrarCarrito();
-// });
-
-// //Agrega el producto 2 a localStorage
-// boton2.addEventListener("click", () => {
-//   const producto = productos.find((item) => {
-//     return item.id === +boton2.dataset.id;
-//   });
-//   carrito.push(producto);
-//   localStorage.setItem("carrito", JSON.stringify(carrito));
-//   mostrarCarrito();
-// });
-
-// //Agrega el producto 3 a localStorage
-// boton3.addEventListener("click", () => {
-//   const producto = productos.find((item) => {
-//     return item.id === +boton3.dataset.id;
-//   });
-//   carrito.push(producto);
-//   localStorage.setItem("carrito", JSON.stringify(carrito));
-//   mostrarCarrito();
-// });
-
-//
-const accesoriosContainer = document.getElementById("accesorios");
-accesoriosContainer.addEventListener("click", function(event) {
-  const button = event.target;
-  if (button.classList.contains("btn-agregar")) {
-  carrito.push(producto);
-  localStorage.setItem("carrito", JSON.stringify(carrito));
-  mostrarCarrito();
-  }
+btnsAgregar.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const id = btn.dataset.id;
+    const producto = productos.find((p) => p.id === parseInt(id));
+    carrito.push(producto);
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+    mostrarCarrito();
+  });
 });
 
 function mostrarCarrito() {
-  const tabla = document.getElementById("producto");
-  tabla.innerHTML = ``;
-  let counter = 1;
-  let productosEnStorage = JSON.parse(localStorage.getItem("carrito"));
+  const carritoContainer = document.getElementById("carrito");
+  carritoContainer.innerHTML = "";
 
+  // Obtener productos del carrito desde el localStorage
+  const productosEnStorage = JSON.parse(localStorage.getItem("carrito")) || [];
+
+  // Mostrar productos del carrito
   productosEnStorage.forEach((producto) => {
-    tabla.innerHTML += `
-            <tr>
-              <th>${counter}</th>
-              <td>${producto.id}</td>
-              <td>${producto.nombre}</td>
-              <td>$${producto.precio}</td>
-              <td>${producto.cantidad}</td>
-              
-            </tr>
-          `;
-    counter++;
+    const productoHTML = `
+      <div>
+        <h3>${producto.nombre}</h3>
+        <p>Precio: ${producto.precio}</p>
+      </div>
+    `;
+    carritoContainer.innerHTML += productoHTML;
   });
-  tr = document.createElement("tr");
-  tr.innerHTML = `<th>TOTAL</th>
-                      <td></td>
-                      <td></td>
-                      <td>$${carrito.reduce(
-                        (total, item) =>
-                          total + (item && item.precio ? item.precio : 0),
-                        0
-                      )}</td>
-                      <td></td>
-                    </tr>`;
-  tabla.appendChild(tr);
+
+  // Mostrar total de la compra
+  const totalHTML = `<p>Total: ${calcularTotal(productosEnStorage)}</p>`;
+  carritoContainer.innerHTML += totalHTML;
 }
 
-mostrarCarrito();
-
-document.addEventListener("DOMContentLoaded", function() {
-  mostrarCarrito();
-});
+function calcularTotal(productos) {
+  return productos.reduce((total, producto) => total + producto.precio, 0);
+}
