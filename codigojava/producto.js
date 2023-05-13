6dd862555b705cecc067bd8fd2c380a304cfb0ec
Dielkis Gutierrez <dielkiag@gmail.com>
2023-05-12 22:51:27 -0300

    Revert "cambios2"
    This reverts commit bf31116b4effccced4b238f59ff8f8edf01acf11.

--- codigojava/producto.js
+++ codigojava/producto.js
@@ -19,8 +19,4 @@ const productos = [
   new Producto(1, "Rolineras", 500, 1),
   new Producto(2, "Valvulas", 25, 1),
   new Producto(3, "Eje trasero", 196.4, 1),
-  new Producto(4, "Rolinera", 200, 1),
-  new Producto(5, "Kit de reparación", 30, 1),
-  new Producto(6, "Catalina de bicicleta", 450, 1),
-  new Producto(7, "Eje Delantero", 950, 1),
 ];
